7da6bb6db6e0ae3ff4731f0ccf8afdc045944567
David Mayes <davidrmayes@gmail.com>
2019-06-02 22:02:10 -0600

    implementing useState and useEffect

--- src/components/recipe-home/recipe-home.js
+++ src/components/recipe-home/recipe-home.js
@@ -1,37 +1,117 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './recipe-home.scss';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
+import Card from '@material-ui/core/Card';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
 
-class RecipeHome extends Component {
-    componentDidMount() {
-        this.props.getCategories();
-    }
+const RecipeHome = props => {
+    const [tabIndex, setTabIndex] = useState(0);
+    const [categories, setCategories] = useState([]);
+    const [areas, setAreas] = useState([]);
+    const [ingredients, setIngredients] = useState([]);
 
-    render() {
-        return (
-            <div className="rh-main-container">
-                <div className="rh-category-container">
-                    {this.props.categories.map((recipe, index) => {
-                        return <div key={index}>{recipe.strCategory}</div>
-                    })}
-                </div>
-    
-                <div className="rh-meals-container">
-    
-                </div>
-    
-                <div className="rh-meal-container">
-    
-                </div>
+
+    useEffect(() => {
+        axios.all([
+            axios.get(`https://www.themealdb.com/api/json/v1/1/list.php?c=list`),
+            axios.get(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`),
+            axios.get(`https://www.themealdb.com/api/json/v1/1/list.php?i=list`),
+        ]).then(data => {
+            console.log(data);
+            setCategories(data[0].data.meals);
+            setAreas(data[1].data.meals);
+            setIngredients(data[2].data.meals);
+            console.log(categories);
+        },)
+    }, [])
+
+    return (
+        <div className="rh-main-container">
+            <Card className="rh-category-container" style={{backgroundColor: '#fffde8'}}>
+                {/* {categories.map((recipe, index) => {
+                    return <div key={index}>{recipe.strCategory}</div>
+                })} */}
+                <Tabs
+                    value={0}>
+                    <Tab label="Categories" />
+                    <Tab label="Areas" />
+                    <Tab label="Ingredients" />
+                </Tabs>
+            </Card>
+
+            <div className="rh-meals-container">
+
             </div>
-        )
-    }
+
+            <div className="rh-meal-container">
+
+            </div>
+        </div>
+    )
 }
 
-const mapStateToProps = ({recipes}) => {
-    console.log(recipes);
-    return recipes;
-};
+function TabContainer(props) {
+    return (
+      <Typography component="div" style={{ padding: 8 * 3 }}>
+        {props.children}
+      </Typography>
+    );
+}
 
-export default connect(mapStateToProps, actions)(RecipeHome);
+export default RecipeHome;
+
+
+
+
+
+// class RecipeHome extends Component {
+//     const [tabIndex, setTabIndex] = useState(0);
+//     componentDidMount() {
+//         this.props.getCategories();
+//     }
+
+//     render() {
+//         return (
+//             <div className="rh-main-container">
+//                 <Card className="rh-category-container" style={{backgroundColor: '#fffde8'}}>
+//                     {/* {this.props.categories.map((recipe, index) => {
+//                         return <div key={index}>{recipe.strCategory}</div>
+//                     })} */}
+//                     <Tabs
+//                         value={0}>
+//                         <Tab label="Categories" />
+//                         <Tab label="Areas" />
+//                         <Tab label="Ingredients" />
+//                     </Tabs>
+//                 </Card>
+    
+//                 <div className="rh-meals-container">
+    
+//                 </div>
+    
+//                 <div className="rh-meal-container">
+    
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// function TabContainer(props) {
+//     return (
+//       <Typography component="div" style={{ padding: 8 * 3 }}>
+//         {props.children}
+//       </Typography>
+//     );
+//   }
+
+// const mapStateToProps = ({recipes}) => {
+//     console.log(recipes);
+//     return recipes;
+// };
+
+// export default connect(mapStateToProps, actions)(RecipeHome);
